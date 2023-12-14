07f0e42f4b2405e2813fad420ea7d74eb273584f
Albert Claudio <117605854+albert-claudio@users.noreply.github.com>
2023-12-14 09:44:27 -0300

    Add files via upload

---
+++ Software.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+void cabecalho(){
+    printf("==============================================\n");
+    printf("                 OPUS LIBRARY                 \n");
+    printf("==============================================\n");
+}
+
+struct login_funcionarios
+{
+    int senhas;
+    float DataDeNascimento;
+    char nome[250];
+};
+
+struct Livros 
+{
+    
+};
